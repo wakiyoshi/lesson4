6a72fd41198aec935cdae2f1d9880f92f20005f0
yoshiwaki <wakk12107@au.com>
2021-10-29 16:15:51 +0900

    quizデータの取得

--- lesson.js
+++ lesson.js
@@ -1,12 +1,52 @@
 
-fetch("https://opentdb.com/api.php?amount=10",{
-  method: 'GET',
-  // mode: 'same-origin',
-  // credentials: 'include'
-})
-.then(response => response.json())
-.then(text => console.log(text))
-.catch((error) => console.log(error));
-
-    
   
+  
+fetchQuizData = async () => {
+  
+  getData = () =>{
+  const getData =  document.createElement('h1');
+  getData.textContent = "取得中"
+  const getting = document.getElementById('get');
+  
+  getting.appendChild(getData);
+  }
+  await getData();
+  
+  const response = await fetch("https://opentdb.com/api.php?amount=10");
+  const quizData = await response.json();
+  const quiz = new Quiz(quizData);
+  quiz.getQuizCategory;
+
+}
+fetchQuizData();
+
+class Quiz {
+  constructor(quizData) {
+   this._quizzes = quizData.results; //クイズデータ
+   this._correctAnswersNum = 0;  //正解数
+
+ }
+
+ 
+ getQuizCategory(index) {
+   return this._quizzes[index - 1].category;
+ }
+ getQuizDifficulty(index){
+   return this._quizzes[index - 1].difficulty;
+ }
+ getQuizQuestion(index){
+  return this._quizzes[index - 1].question;
+ }
+ getQuizCorrectAnswer(index){
+  return this._quizzes[index - 1].correct_answer;
+ }
+ getQuizIncorrectAnswer(index){
+  return this._quizzes[index - 1].incorrect_answers;//arrayを取得
+ }
+ getQuizCorrectAnswer(index){
+  return this._quizzes[index - 1].correct_answer;
+ }
+
+}
+
+
