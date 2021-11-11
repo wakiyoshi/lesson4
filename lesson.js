e1e219a0f841ed753fee8d1e56f1cbe45ebf74c5
yoshiwaki <wakk12107@au.com>
2021-11-11 19:18:59 +0900

    機能を一通り実装しました

--- lesson.js
+++ lesson.js
@@ -13,45 +13,17 @@ const fetchQuizData = async (index) => {
   const response = await fetch("https://opentdb.com/api.php?amount=10");
   const quizData = await response.json();
   const quiz = new Quiz(quizData);
+  setNextQuiz(quiz, index);
   
   console.log(quizData);
-    //問題文の表示
-  title.innerText = `問題${index}`;
+  console.log(quiz.getCorrectAnswersNum)
 
-  const genre = document.getElementById('genre');
-  genre.innerText = `【ジャンル】 ${quiz.getQuizCategory(1)}`;
-  
-  const difficulty = document.getElementById('difficulty');
-  difficulty.innerText = `【難易度】 ${quiz.getQuizDifficulty(1)}`;
+}
 
-  const question = document.getElementById('questions');
-  question.innerText = ` ${quiz.getQuizQuestion(1)}`;
-
-  const correctAnswers = quiz.getQuizCorrectAnswer(1);
-  const incorrectAnswers = quiz.getQuizIncorrectAnswer(1);
-
-  //問題のシャッフル
-  const shuffle = ([...array]) => {
-    for (let i = array.length - 1; i >= 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
-  
-  for(let i = 0; i < 10; i++){
-  console.log(shuffle(...correctAnswers,...incorrectAnswers));}
-//問題をクリックした時の正誤判定
-  button.addEventListener('click', ()=>{
-    quiz.countCorrectAnswers(index);
-      index++;
-
-    setNextQuiz(quiz,index);
-
-  })
-//問題終了か、問題続行かの条件分岐
-  const answersArea = document.getElementById('answers');
   const setNextQuiz = (quiz, index) => {
+
+    const answersArea = document.getElementById('answers');
+
     while (answersArea.firstChild) {
       answersArea.removeChild(answersArea.firstChild);
     }
@@ -61,54 +33,111 @@ const fetchQuizData = async (index) => {
       finishQuiz(quiz);
     }
   }
-   
-  //次の問題を表示
   const makeQuiz =  (quiz,index) => {
-    fetchQuizData(quiz,index)
+    const shuffleArray = ([...array]) => {
+      for (let i = array.length - 1; i >= 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
     }
-//問題を終了して正解数を表示
-    const finishQuiz = (quiz) =>{
-      
+    const buildAnswers = () => {
+      const answers = [
+        quiz.getQuizCorrectAnswer(index),
+        ...quiz.getQuizIncorrectAnswers(index)
+      ];
+      return shuffleArray(answers);
     }
-}
-
-
-class Quiz {
-  constructor(quizData) {
-   this._quizzes = quizData.results; //クイズデータ
-   this._correctAnswersNum = 0;  //正解数
- }
-
- getNumQuizzes(index) {
-   return this._quizzes[index]
- }
- getQuizCategory(index) {
-   return this._quizzes[index - 1].category;
- }
- getQuizDifficulty(index){
-   return this._quizzes[index - 1].difficulty;
- }
- getQuizQuestion(index){
-  return this._quizzes[index - 1].question;
- }
- getQuizCorrectAnswer(index){
-  return this._quizzes[index - 1].correct_answer;
- }
- getQuizIncorrectAnswer(index){
-  return this._quizzes[index - 1].incorrect_answers;//arrayを取得
- }
- countCorrectAnswers(index,answer){
-  if(this._quizzes[index - 1].correct_answer  === answer){
-    this._correctAnswersNum++
-  }
+    buildAnswers();
   
-}
- getCorrectAnswersNum(){
- return this.__correctAnswersNum
- }
+    const answersArea = document.getElementById('answers');
+    shuffleArray(buildAnswers()).forEach((a) => {
+      const button = document.createElement('button')
+      button.innerText = a;
+      const br = document.createElement('br');
+      answersArea.appendChild(button)
+      answersArea.appendChild(br)
 
+      button.addEventListener('click', answer =>{
+          answer = a;
+          quiz.countCorrectAnswers(index,answer);
+            index++;
+      console.log(quiz.getCorrectAnswersNum())
+          setNextQuiz(quiz,index);
+          
+      })
+    });
+    title.innerText = `問題${index}`;
+    
+    const genre = document.getElementById('genre');
+    genre.innerText = `【ジャンル】 ${quiz.getQuizCategory(index)}`;
+    genre.style.display = 'block'
+    
+    const difficulty = document.getElementById('difficulty');
+    difficulty.innerText = `【難易度】 ${quiz.getQuizDifficulty(index)}`;
+    difficulty.style.display = 'block'
 
-}
+    const question = document.getElementById('questions');
+    question.innerText = ` ${quiz.getQuizQuestion(index)}`;
+  }
+  const finishQuiz = (quiz) =>{
+    const answersArea = document.getElementById('answers');
+        
+        title.innerText = `あなたの正解数は${quiz.getCorrectAnswersNum()}です！！！`;
+    
+        const genre = document.getElementById('genre');
+        genre.style.display = 'none';
+        
+        const difficulty = document.getElementById('difficulty');
+        difficulty.style.display = 'none';
+      
+        const question = document.getElementById('questions');
+        question.innerText = `もう一度挑戦したい場合は以下をクリック！！`;
+        
+  
+        const restartButton = document.createElement('button');
+        restartButton.innerText = 'ホームへ戻る'
+        restartButton.addEventListener('click', ()=>{
+          document.getElementById("start").style.display = "block";
+          restartButton.hidden = 'true';
+          title.innerText = 'ようこそ'
+          question.innerText = `以下のボタンをクリック`;
+        });
+        answersArea.appendChild(restartButton);
+      }
+      
+      class Quiz {
+        constructor(quizData) {
+         this._quizzes = quizData.results; //クイズデータ
+         this._correctAnswersNum = 0;  //正解数
+       }
 
-
-
+       getQuizCategory(index) {
+         return this._quizzes[index - 1].category;
+       }
+       getQuizDifficulty(index){
+         return this._quizzes[index - 1].difficulty;
+       }
+       getQuizQuestion(index){
+        return this._quizzes[index - 1].question;
+       }
+       getQuizCorrectAnswer(index){
+        return this._quizzes[index - 1].correct_answer;
+       }
+       getQuizIncorrectAnswers(index){
+        return this._quizzes[index - 1].incorrect_answers;//arrayを取得
+       }
+       countCorrectAnswers(index,answer){
+        if(this._quizzes[index - 1].correct_answer  === answer
+          ){
+          this._correctAnswersNum++
+        }
+      }
+        getNumQuizzes() {
+          return this._quizzes.length;
+        }
+       
+       getCorrectAnswersNum(){
+       return this._correctAnswersNum;
+       }
+      }
